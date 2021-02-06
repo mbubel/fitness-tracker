5cf7cbbc4abbee3e7e62e3c06b4f4903bdb07782
Michael <michaelbubel.1989@gmail.com>
2021-02-05 21:58:45 -0800

    Added GET and POST routes

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "exercise.html"));
+});
+
+app
+  .route("/api/workouts")
+  .get((req, res) => {
+    db.Workout.find({})
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  })
+  .post(({ body }, res) => {
+    db.Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+        console.log(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
