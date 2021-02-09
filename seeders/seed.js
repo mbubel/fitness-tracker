3238dd10948609dd6b966fa1bc539f82ad15fa5a
Michael <michaelbubel.1989@gmail.com>
2021-02-08 16:41:32 -0800

    Updated Seeders

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/cluster0',
+  process.env.MONGODB_URI || 'mongodb://localhost/Cluster0',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
